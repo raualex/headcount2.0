dfdb5b5cc4909790ad81a527d8c20d1f3bcf51c0
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-06 14:39:10 -0600

    Complete Card tests and add ComparisonCard component

--- src/Card.test.js
+++ src/Card.test.js
@@ -24,3 +24,26 @@ it('gives class of: greater if above 0.5', () => {
   ).toEqual(true);  
 });
 
+it('gives class of: lesser if below 0.5', () => {
+  const wrapper = shallow(<Card 
+    key={'COLORADO'}
+    schoolName={'COLORADO'}
+    schoolInfo={{2004: 0.25}}
+  />);
+
+  expect(
+    wrapper.find('.data-list').children().first().hasClass('lesser')
+  ).toEqual(true);
+});
+
+it('gives class of: equal if euqal to 0.5', () => {
+  const wrapper = shallow(<Card 
+    key={'COLORADO'}
+    schoolName={'COLORADO'}
+    schoolInfo={{2001: 0.5}}
+  />);
+
+  expect(
+    wrapper.find('.data-list').children().first().hasClass('equal')
+  ).toEqual(true);
+});
---
+++ src/ComparisonCard.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ComparisonCard from './ComparisonCard';
+
+it('matches the snapshot', () => {
+
+});
